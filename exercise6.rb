bd2d6d6af48c0de1ecfbbd6d9b0b032592831903
Fernando Ikeda <f_ikeda10@hotmail.com>
2015-06-09 16:46:03 -0400

    Exercise 6 till point number 5 without point number 3

--- exercise6.rb
+++ exercise6.rb
@@ -1,1 +1,19 @@
-grocery_list = ["chicken", "eggs", "nuggets", "hamburgers"]
+grocery_list = ["chicken", "eggs", "nuggets", "hamburgers"] 
+
+grocery_list.each do |x|
+	puts "* #{x}" 
+end
+
+grocery_list << "rice"
+
+
+puts grocery_list.length
+
+if grocery_list.include?("banana")
+	puts "You need to pick up bananas"
+else
+	puts "You don´t need to pick up bananas"
+end
+
+puts grocery_list[1]
+
