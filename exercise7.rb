a98d832b69c892e6e8f4dad9a848cde9ba2b27e7
Fernando Ikeda <f_ikeda10@hotmail.com>
2015-06-10 14:25:52 -0400

    Exercise 7 finished without bonus

--- exercise7.rb
+++ exercise7.rb
@@ -18,3 +18,20 @@ students[:cohort4] = 43
 "\n"
 puts "Cohort Names:"
 puts students.keys
+
+"\n"
+puts "New Cohort sizes after expansion:"
+def cohort(expansion)
+	expansion.each do |cohort, size|
+		puts "#{cohort.capitalize}: #{size.to_i * 1.05} students"
+	end
+end
+
+puts cohort(students)
+
+"\n"
+students.delete(:cohort2)
+puts "New Cohort list without Cohort 2:"
+puts print_cohorts(students)
+
+
