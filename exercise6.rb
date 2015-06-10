e2924dcc502a366bca34d2adf5bbdf0bb48814fd
Fernando Ikeda <f_ikeda10@hotmail.com>
2015-06-10 09:58:40 -0400

    everything till question 6

--- exercise6.rb
+++ exercise6.rb
@@ -1,19 +1,40 @@
-grocery_list = ["chicken", "eggs", "nuggets", "hamburgers"] 
-
-grocery_list.each do |x|
+#method to print (question 2)
+def grocery_print(grocerylist)
+grocerylist.each do |x|
 	puts "* #{x}" 
 end
+end
 
+grocery_list = ["chicken", "eggs", "nuggets", "hamburgers", "salmon"] 
+
+#question 1 using the method 
+puts "My grocery list is:"
+grocery_print(grocery_list)
+
+
+puts"\n"
 grocery_list << "rice"
 
+puts "My grocery list is:"
+grocery_print(grocery_list)
 
-puts grocery_list.length
+#amount of product in grocery list (question 3)
+puts"\n"
+puts "The grocery list length is: #{grocery_list.length}"
 
+#Question 4 - checking if bananas is part of the grocery list
+puts"\n"
 if grocery_list.include?("banana")
 	puts "You need to pick up bananas"
 else
 	puts "You don´t need to pick up bananas"
 end
 
-puts grocery_list[1]
+puts"\n"
+puts "The second item in the grocery list are: #{grocery_list[1]}."
 
+puts"\n"
+puts "My grocery list sorted alphabetically is:"
+grocery_list.sort.each do |x|
+	puts "* #{x}" 
+end
